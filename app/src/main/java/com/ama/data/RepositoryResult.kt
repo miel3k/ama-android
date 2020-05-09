dac77228305eee93e9088ec80c639c39325f0556
miel3k <kuba.mielczarek96@icloud.com>
2020-05-09 12:27:52 +0200

    Remove unused class from RepositoryResult

--- app/src/main/java/com/ama/data/RepositoryResult.kt
+++ app/src/main/java/com/ama/data/RepositoryResult.kt
@@ -1,8 +1,6 @@
 package com.ama.data
 
 sealed class RepositoryResult<out R> {
-
     data class Success<out T>(val data: T) : RepositoryResult<T>()
     data class Error(val exception: Exception) : RepositoryResult<Nothing>()
-    object Loading : RepositoryResult<Nothing>()
 }
